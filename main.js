96f3e36890e6d9e07f59fff276acd813ba01105b
Sophia Barros <soohbarros1@gmail.com>
2025-05-31 23:05:16 -0300

    criação do main.js e criação da função para adicionar itens ao array items.

---
+++ main.js
@@ -0,0 +1,15 @@
+const items = []
+
+function addItem() {
+    const itemName = document.querySelector("#item").value
+
+    const item = {
+        name: itemName,
+        checked: false
+    }
+
+    items.push(item)
+
+    console.log(item)
+    document.querySelector("#item").value = ""
+}
